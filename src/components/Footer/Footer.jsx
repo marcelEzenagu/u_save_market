316e73afe8dc7981a4d498ff1213f973c24bfdd5
Marcel <marcelezenagu92@gmail.com>
2025-04-03 23:05:30 +0100

    fix: continued fixes found

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,48 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import FooterImage from '../../assets/images/Footer/footer.webp'
+import React from "react";
+import { Link } from "react-router-dom";
+import FooterImage from "../../assets/images/Footer/footer.webp";
 import { FaSquareFacebook } from "react-icons/fa6";
 import { SlSocialInstagram } from "react-icons/sl";
 import { BsTwitter } from "react-icons/bs";
-import { FaLinkedinIn } from "react-icons/fa";
+// import { FaLinkedinIn } from "react-icons/fa";
 function Footer() {
   return (
     <div>
-        <footer className='w-full p-3 md:p-5 flex flex-col md:flex-row  items-start md:items-end justify-between '>
-            <div className='flex flex-col md:flex-row items-center md:items-end  gap-4'>
-            <img src={FooterImage} alt="" className='w-[170px]' />
-            <ul  className='md:flex grid flex-col md:flex-row items-center mb-4  gap-4'>
-                <li className='text-[12px] text-regal-light-gray font-[500]'>
-                    <Link>&copy; 2024</Link>
-                </li>
-                <li className='text-[12px] text-regal-light-gray font-[500]'>
-                    <Link>Terms & Conditions</Link>
-                </li>
-                <li className='text-[12px] text-regal-light-gray font-[500]'>
-                    <Link>Privacy</Link>
-                </li>
-                <li className='text-[12px] text-regal-light-gray font-[500]'>
-                    <Link>Help</Link>
-                </li>
-            </ul>
-            </div>
-            <div className='flex flex-row items-center gap-4 mb-3'>
-                <Link className='text-xl text-regal-light-gray font-[500]'>
-                <FaSquareFacebook />
-                </Link>
-                <Link className='text-xl text-regal-light-gray font-[500]'>
-                <SlSocialInstagram />
-                </Link>
-                <Link className='text-xl text-regal-light-gray font-[500]'>
-                <BsTwitter />
-                </Link>
-                <Link className='text-xl text-regal-light-gray font-[500]'>
+      <footer className="w-full p-3 md:p-5 flex flex-col md:flex-row  items-start md:items-end justify-between ">
+        <div className="flex flex-col md:flex-row items-center md:items-end  gap-4">
+          <img src={FooterImage} alt="" className="w-[170px]" />
+          <ul className="md:flex grid flex-col md:flex-row items-center mb-4  gap-4">
+            <li className="text-[12px] text-regal-light-gray font-[500]">
+              <Link>&copy; 2024</Link>
+            </li>
+            <li className="text-[12px] text-regal-light-gray font-[500]">
+              <Link>Terms & Conditions</Link>
+            </li>
+            <li className="text-[12px] text-regal-light-gray font-[500]">
+              <Link>Privacy</Link>
+            </li>
+            <li className="text-[12px] text-regal-light-gray font-[500]">
+              <Link>Help</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex flex-row items-center gap-4 mb-3">
+          <Link className="text-xl text-regal-light-gray font-[500]">
+            <FaSquareFacebook />
+          </Link>
+          <Link className="text-xl text-regal-light-gray font-[500]">
+            <SlSocialInstagram />
+          </Link>
+          <Link className="text-xl text-regal-light-gray font-[500]">
+            <BsTwitter />
+          </Link>
+          {/* <Link className='text-xl text-regal-light-gray font-[500]'>
                 <FaLinkedinIn />
-                </Link>
-            </div>
-        </footer>
+                </Link> */}
+        </div>
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
